058ab24df4689de1ffcc269953607b799f16090a
Joey <joey@evolution-x.org>
2024-04-06 03:27:27 +0900

    marble: Initialize for Evolution X 9.x

--- lineage_marble.mk
+++ lineage_marble.mk
@@ -10,10 +10,16 @@ $(call inherit-product, device/xiaomi/marble/device.mk)
 # Inherit from common lineage configuration
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+EVO_BUILD_TYPE := Official
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BUILD_APERTURE_CAMERA := false
+
+# Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_marble
 PRODUCT_DEVICE := marble
 PRODUCT_MANUFACTURER := Xiaomi
 
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := POCO/marble_global/marble:12/SKQ1.230401.001/V816.0.5.0.UMRMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
