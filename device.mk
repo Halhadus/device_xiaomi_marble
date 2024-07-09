df0f6fee3b89d40a3fcbce458bb116a630f26980
Halhadus <eneshakans45@proton.me>
2024-07-09 17:01:39 +0000

    Update device.mk

--- device.mk
+++ device.mk
@@ -19,6 +19,9 @@ $(call inherit-product, vendor/xiaomi/marble/marble-vendor.mk)
 # Call the MiuiCamera setup
 $(call inherit-product-if-exists, device/xiaomi/miuicamera-marble/device.mk)
 
+# Lindroid
+$(call inherit-product, vendor/lindroid/lindroid.mk)
+
 # Init scripts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.marble.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.marble.rc \
